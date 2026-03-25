73135c37b8a07cd533740cb5b043e18935877ea6
jlorenagonzalez <167938841+jlorenagonzalez@users.noreply.github.com>
2026-03-25 01:22:42 -0500

    Linear regression house price prediction

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, render_template, request
+import house_price
 
 app = Flask(__name__)
 
@@ -23,6 +24,7 @@ def case3():
 def case4():
     return render_template('case4.html')
 
+
 @app.route('/concepts')
 def concepts():
     return render_template('concepts.html')
@@ -36,6 +38,37 @@ def application():
         resultado = LinealRegresion.calculateGrade(hours)
 
     return render_template('application.html', resultado=resultado)
+
+@app.route('/application_linear', methods=["GET","POST"])
+def application_linear():
+
+    result = None
+
+    if request.method == "POST":
+
+        area = float(request.form["area"])
+        rooms = float(request.form["habitaciones"])
+        bathrooms = float(request.form["banos"])
+        floors = float(request.form["pisos"])
+        garage = float(request.form["garaje"])
+        age = float(request.form["antiguedad"])
+        distance = float(request.form["distancia"])
+
+        result = house_price.predict_price(
+            area,
+            rooms,
+            bathrooms,
+            floors,
+            garage,
+            age,
+            distance
+        )
+
+    return render_template(
+        'linear_regre_app.html',
+        result=result
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
